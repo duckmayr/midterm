c040b3bd941a066b2e5183307eb13f374bf57b1e
JB Duck-Mayr <j.duckmayr@gmail.com>
2017-03-23 20:37:20 -0500

    add R file to document package, update DESCRIPTION

---
+++ raschR/R/raschR-package.R
@@ -0,0 +1,9 @@
+#' squaresPack
+#'
+#' The squaresPack package calculates sums and differences of squared values.  
+#' @name raschR
+#' @docType package
+#' @author  JB Duck-Mayr
+#' @importFrom graphics abline lines
+#' @importFrom stats dnorm integrate
+NULL
