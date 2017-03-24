c269fc77e6dc6de176fd384524b9d9913b92f867
JB Duck-Mayr <j.duckmayr@gmail.com>
2017-03-23 23:31:16 -0500

    update import statments and demo index

--- easyRasch/R/easyRasch-package.R
+++ easyRasch/R/easyRasch-package.R
@@ -4,7 +4,9 @@
 #' @name easyRasch
 #' @docType package
 #' @author  JB Duck-Mayr
-#' @importFrom graphics abline lines
+#' @importFrom graphics abline lines legend
+#' @importFrom grDevices palette
 #' @importFrom stats dnorm integrate
-#' @importFrom lattice strip.custom xyplot
+#' @importFrom lattice strip.custom xyplot panel.abline panel.xyplot
+#' @import methods
 NULL
